22893b0523c59614467628d5c721dc7bb470cbfc
zhugg <emma7727@163.com>
2018-09-10 14:44:14 +0800

    idea导致冲突了

--- demo/src/main/java/com/zl/controller/UserController.java
+++ demo/src/main/java/com/zl/controller/UserController.java
@@ -21,7 +21,7 @@ public class UserController {
     @RequestMapping("/selectall")
     @ResponseBody
     public Object selectall(){
-    	  System.out.println("idea提交");
+    	  System.out.println("idea导致冲突");
         return userService.selectall();
     }
 
